1b550377b646c425a3f892b41195a172ef288385
diegomed <diegomedina92@gmail.com>
2023-04-04 23:57:47 -0700

    sorting tasks according to dates and filtering only upcoming tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,13 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 const tasks = [];
 
 export const getTasks = (req, res, next) => {
-    res.status(200).json(tasks);
+    const today = new Date();    
+    const tasksSinceToday = tasks.filter(t => new Date(t.date) > new Date() || new Date(t.date).toLocaleDateString('es-ES', { timeZone:"UTC"}) === new Date().toLocaleDateString('es-ES', { timeZone:"UTC"})); // This was done to compare exact same dates
+    res.status(200).json(tasksSinceToday);
 };
 
 export const postTask = (req, res, next) => {
     try {
         const task = {...req.body, id: uuidv4()}
         tasks.push(task);
+        tasks.sort((a, b) => new Date(a.date) - new Date(b.date));
     }
     catch(err) {
         res.sendStatus(500);
